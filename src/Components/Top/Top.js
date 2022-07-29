d0c6c3128a61fee17d9786e7920c6a2a1067fc2b
Miguel Faria <mattar1704miguel@gmail.com>
2022-07-29 15:19:57 -0300

    Bonus: botão voltar

--- src/Components/Top/Top.js
+++ src/Components/Top/Top.js
@@ -1,8 +1,13 @@
 import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Top() {
+    const navigate = useNavigate();
+    const back = "<";
+    
     return (
         <div className="top">
+            <div className="back" onClick={() => navigate(-1)}>{back}</div>
             CINEFLEX
         </div>
     ); 
